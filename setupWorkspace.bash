4e9a2b98d78cf2ee24ec6ed01b3e6b4182d54ef0
gaiapjo <posborne@ast.cam.ac.uk>
2026-01-12 12:58:33 +0000

    Avoid duplicate IngestSpark jars in the classpath.

--- setupWorkspace.bash
+++ setupWorkspace.bash
@@ -8,19 +8,20 @@ PP="$ROOT/photpipe"
 WS="$ROOT/workspace"
 PP_VERSION="22.3.0-SNAPSHOT"
 
-echo "Building photpipe for ./workspace..."
+echo "Building PhotPipe..."
 pushd $PP
 mvn clean package -Dmaven.test.skip=true -P Bundle "$@"
 popd
 
+echo "Configuring workspace..."
 mkdir -p $WS
 rm -f $WS/*bundle.zip $WS/*.jar IngestSpark*.jar
 rm -rf $WS/lib
-cp $PP/Bundle/target/Bundle-22.3.0-SNAPSHOT-bundle.zip $WS/
-cp $PP/./PhotPipeIngest/IngestSpark/target/IngestSpark-22.3.0-SNAPSHOT.jar $WS/
+cp $PP/Bundle/target/Bundle-${PP_VERSION}-bundle.zip $WS/
 
 pushd $WS
 unzip -j *bundle.zip -d ./lib
+mv ./lib/IngestSpark-${PP_VERSION}.jar ./
 popd
 
 echo "... done!"
